6153ec3829196c1ed6202ed2d3107ff06cec8166
Selinne Carlin <75504946+LynnNoOne@users.noreply.github.com>
2022-10-22 00:07:39 -0500

    Update 1PROYECT.py

--- 1PROYECT.py
+++ 1PROYECT.py
@@ -1,14 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def inicio():
     return render_template("NEWHTML.html")
-@app.route('/procesar', methods=['POST'])
 
-   
-app.run(debug=True,port=5000)
 
 
 
